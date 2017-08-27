11dd80447497293a233c380e87d8de0ab8a78f83
Gregor <gregor@martynus.net>
2017-08-27 00:32:45 -0700

    feat: intial version

---
+++ index.js
@@ -0,0 +1,23 @@
+const Hapi = require('hapi')
+
+// Create a server with a host and port
+const server = new Hapi.Server()
+server.connection({
+  address: '0.0.0.0',
+  port: 8000
+})
+
+// Add the route
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    return reply('hapi world!')
+  }
+})
+
+// Start the server
+server.start((error) => {
+  if (error) throw error
+  console.log('Server running at:', server.info.uri)
+})
